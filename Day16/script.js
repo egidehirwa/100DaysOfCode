b2b38a918dbf4fc1fcf38b210f757d53f26eb4bb
Egide Hirwa <66557478+egidehirwa@users.noreply.github.com>
2024-02-16 23:45:21 +0100

    day 16 - account dropdown

---
+++ Day16/script.js
@@ -0,0 +1,11 @@
+const toggle = document.getElementsByClassName('account__user-wrapper');
+const userCard = document.getElementsByClassName('account-card');
+
+toggle.onclick = function() {
+    if (userCard.style.display !== "none") {
+        userCard.style.display !== "none"
+    } else {
+        userCard.style.display !== "block"
+    }
+}
+
